08552b3cffcad36a781e99d8cdcf265be037ac54
99omniaashraf <omniaashraf249@gmail.com>
2023-09-17 12:16:03 -0700

    I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

---
+++ _printf.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <limits.h>
+#include "main.h"
+/**
+ * _printf - produces output according to a format
+ * @format: is a character string
+ * @...: variable
+ * Return: the number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	int count = 0;
+	va_list prts;
+
+	va_start(prts, format);
+
+	if (format[count] == '%')
+	{
+		count++;
+		while (format[count] <= 48)
+		{
+			switch (format[count])
+			{
+				case 'c':
+					printf("\%c", va_arg(prts, int));
+					break;
+				case 's':
+					printf("\%s", va_arg(prts, char*));
+					break;
+				case '%':
+					printf("\%%", va_arg(prts, void*));
+					break;
+				default:
+					putchar(format[count]);
+			}
+		}
+	}
+	else
+	{
+		putchar(format[count]);
+	}
+	count++;
+	va_end(prts);
+	return (count);
+}
---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("Character:[%c]\n", 'H');
+	printf("String:[%s]\n", "I am a string !");
+	printf("Percent:[%%]\n");
+	return (0);
+}

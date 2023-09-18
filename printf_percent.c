86e4ebcee1d0114ac607545d1851470a66e8069a
MariaRamzy <maria_botros94@hotmail.com>
2023-09-18 13:12:30 -0700

    printf

--- printf_percent.c
+++ printf_percent.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_percent - Prints a percent symbol
+ * printf_percent - Prints a percent symbol
  * @list: list of arguments
  * Return: Will return the amount of characters printed.
  */

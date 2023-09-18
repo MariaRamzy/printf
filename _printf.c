f96dd32c39ac9201b210fbd8e564c9fafa359a51
99omniaashraf <omniaashraf249@gmail.com>
2023-09-18 10:39:25 -0700

    printf

---
+++ _printf.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _printf - produces output according to a format.
+ * @format: a character string
+ * @...: variable
+ * Return: the number of characters printed
+ * (excluding the null byte used to end output to strings)
+ */
+int _printf(const char *format, ...)
+{
+	unsigned int count;
+	unsigned int a = 0;
+	va_list args;
+
+	va_start(args, format);
+	while (format[a] != '\0')
+	{
+		a++;
+		if (!format)
+		{
+			return (-1);
+
+			switch (format[a])
+			{
+				case 'c':
+					printf("%c", va_arg(args, int));
+					break;
+				case 's':
+					printf("%s", _strlen(va_arg(args, char*)));
+					break;
+				case '%':
+					putchar('%');
+					break;
+				default:
+					putchar(format[a]);
+					break;
+			}
+			a++;
+		}
+		else
+		{
+			count++;
+			putchar(format[a]);
+		}
+	}
+	va_end(args);
+	return (count);
+}
--- main.c
+++ main.c
@@ -7,12 +7,8 @@
  */
 int main(void)
 {
-	_printf("Let's print a simple sentence.\n");
-	printf("Let's print a simple sentence.\n");
-	_printf("%s", "I am a string !\n");
 	printf("%s", "I am a string !\n");
-	_printf("%c\n", 'H');
 	printf("%c\n", 'H');
-	_printf("%%\n");
 	printf("%%\n");
+	return (0);
 }

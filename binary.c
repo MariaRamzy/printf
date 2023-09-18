3ff7b5cb1d5d4809183270963b74d26621f14bd7
MariaRamzy <maria_botros94@hotmail.com>
2023-09-18 15:11:19 -0700

    task 2

---
+++ binary.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+/**
+ * write_base - sends characters to be written on standard output
+ * @str: String to parse
+ */
+
+void write_base(char *str)
+{
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+_putchar(str[i]);
+}
+
+/**
+ * base_len - Calculates the length for an octal number
+ * @num: The number for which the length is being calculated
+ * @base: Base to be calculated by
+ * Return: An integer representing the length of a number
+ */
+
+unsigned int base_len(unsigned int num, int base)
+{
+unsigned int i;
+
+for (i = 0; num > 0; i++)
+{
+num = num / base;
+}
+return (i);
+}
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n; i++)
+dest[i] = src[i];
+dest[i] = '\0';
+return (dest);
+}
+
+
+/**
+ * hex_check - Checks which hex function is calling it
+ * @num: Number to convert into letter
+ * @x: Tells which hex function is calling it
+ * Return: Ascii value for a letter
+ */
+
+int hex_check(int num, char x)
+{
+char *hex = "abcdef";
+char *Hex = "ABCDEF";
+
+num = num - 10;
+if (x == 'x')
+return (hex[num]);
+else
+return (Hex[num]);
+return (0);
+}

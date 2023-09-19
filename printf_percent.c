ac1732ec7d5a0092f5b1cfd426f9b5c5e2a2b4e4
99omniaashraf <omniaashraf249@gmail.com>
2023-09-19 16:28:18 -0700

    printf

--- printf_percent.c
+++ printf_percent.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * printf_percent - Prints a percent symbol
- * @list: list of arguments
+ * printf_percent - Prints a percent symbol.
+ * @list: list of arguments.
  * Return: Will return the amount of characters printed.
  */
 int printf_percent(__attribute__((unused))va_list list)

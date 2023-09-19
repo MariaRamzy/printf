96d560152dc82b1ddd3b6320a4fcd38cbad69115
MariaRamzy <maria_botros94@hotmail.com>
2023-09-19 12:18:38 -0700

    flags

--- main.h
+++ main.h
@@ -7,6 +7,16 @@
 #include <stdarg.h>
 #include <limits.h>
 
+/* FLAGS */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
+
 #define NULL_STRING "(null)"
 #define NUL '\0'
 

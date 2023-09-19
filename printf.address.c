8342565241a3b8812e7034b3523eaffbd23ca776
99omniaashraf <omniaashraf249@gmail.com>
2023-09-19 06:45:57 -0700

    printf

--- printf.address.c
+++ printf.address.c
@@ -1,14 +1,22 @@
 #include "main.h"
 
+/**
+ * printf_address - print as pointer
+ * @val: val of argument
+ * Return: number
+ */
 int printf_address(va_list val)
 {
 	void *p;
+	unsigned int a;
+
+	a = *p;
 
 	if (p == NULL)
 	{
-		printf("%p", va_arg(val, void *)
+		printf("%p", va_arg(val, void *));
 	}
 	_putchar ('x');
 	_putchar ('0');
-	return (p);
+	return (a);
 }
